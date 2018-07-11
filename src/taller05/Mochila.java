e6df81e9ad44bc551546d1d920608ce5dfce0679
Julian Sanchez <julianalejandrosanchez@hotmail.com>
2018-07-10 21:48:17 -0300

    Final

--- src/taller05/Mochila.java
+++ src/taller05/Mochila.java
@@ -10,37 +10,37 @@ public class Mochila {
 		}
 
 	public void resolve(int[] peso, int[] valor, int pesoMaximo){
-		int N=4; //items
-		int W=4; //max peso
+		int N=peso.length; //items
+		int W=peso.length+1; //max peso
 		int[][] opt = new int[N][W]; //matriz que guarda el valor de cada escenario
 		boolean[][] sol = new boolean[N][W]; //guarda si el elemento está presente
-		
-		for (int n=0; n<=N; n++){
+
+		for (int n=0; n<N; n++){
 			for (int w=0; w<W; w++){
 				int option1 = n < 1 ? n : opt[n-1][w];
 				int option2 = Integer.MIN_VALUE;
-				
-				if(peso[n]<=w){ 
+
+				if(peso[n]<=w && n>0){
 						option2 = valor[n]+opt[n-1][w-peso[n]];//ENTRA ACA EN LA PRIMERA ITERACION Y TIRA ERROR FUERA DE RANGO, NO DEBERIA
 				}
-				
+
 				opt[n][w] = Math.max(option1, option2);
 				sol[n][w] = (option2 > option1);
 			}
 		}
-		
+
 		//determinar la combinación óptima
-		
+
 		boolean[] esSolucion = new boolean[N];
-		for (int n = N, w = W; n>=0; n--){
-			if (sol[n-1][w]){
-				esSolucion[n-1] = true;
-				w = w - peso[n-1];
+		for (int n = N-1, w = W-1; n>0; n--){
+			if (sol[n][w]){
+				esSolucion[n] = true;
+				w = w - peso[n];
 			}else{
 				esSolucion [n] = false;
 			}
 		}
-		
+
 		System.out.println("Los objetos optimos son: \n");
 		for(int i=0; i<N; i++){
 			if(esSolucion[i])
